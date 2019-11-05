b04b713968ee4629c9f93de3816709a75cc73d32
Mathias Dannesbo <md@magenta.dk>
2019-11-05 16:46:42 +0100

    Do not require all databases to be configured

--- docker-entrypoint-initdb.d/10-init-lora-data-db.sh
+++ docker-entrypoint-initdb.d/10-init-lora-data-db.sh
@@ -1,51 +1,55 @@
 #!/bin/bash
 set -e
 
-true "${DB_USER:?DB_USER is unset. Error.}"
-true "${DB_PASSWORD:?DB_PASSWORD is unset. Error.}"
-true "${DB_NAME:?DB_NAME is unset. Error.}"
+if [ -n "$DB_NAME" ]; then
 
-# The three following `alter database … set` and `create schema …` commands
-# should be identical the ones in oio_rest/oio_rest/db/management.py used for
-# tests.
+    true "${DB_USER:?DB_USER is unset. Error.}"
+    true "${DB_PASSWORD:?DB_PASSWORD is unset. Error.}"
 
-echo Create $DB_NAME
+    # The three following `alter database … set` and `create schema …` commands
+    # should be identical the ones in oio_rest/oio_rest/db/management.py used for
+    # tests.
 
-psql -v ON_ERROR_STOP=1 <<-EOSQL1
-    create user $DB_USER with encrypted password '$DB_PASSWORD';
-    create database $DB_NAME;
-    grant all privileges on database $DB_NAME to $DB_USER;
-    alter database $DB_NAME set search_path to actual_state, public;
-    alter database $DB_NAME set datestyle to 'ISO, YMD';
-    alter database $DB_NAME set intervalstyle to 'sql_standard';
-    \connect $DB_NAME
-    create schema actual_state authorization $DB_USER;
+    echo Creating LoRA data db: $DB_NAME
+
+    psql -v ON_ERROR_STOP=1 <<-EOSQL1
+        create user $DB_USER with encrypted password '$DB_PASSWORD';
+        create database $DB_NAME;
+        grant all privileges on database $DB_NAME to $DB_USER;
+        alter database $DB_NAME set search_path to actual_state, public;
+        alter database $DB_NAME set datestyle to 'ISO, YMD';
+        alter database $DB_NAME set intervalstyle to 'sql_standard';
+        \connect $DB_NAME
+        create schema actual_state authorization $DB_USER;
 EOSQL1
 
 
-echo Adding extensions
+    echo Adding extensions
 
-# The three following `create extension … ` commands should be identical the
-# ones in oio_rest/oio_rest/db/management.py used for tests.
+    # The three following `create extension … ` commands should be identical the
+    # ones in oio_rest/oio_rest/db/management.py used for tests.
 
-psql -v ON_ERROR_STOP=1 -d $DB_NAME <<-EOSQL2
-    create extension if not exists "uuid-ossp" with schema actual_state;
-    create extension if not exists "btree_gist" with schema actual_state;
-    create extension if not exists "pg_trgm" with schema actual_state;
+    psql -v ON_ERROR_STOP=1 -d $DB_NAME <<-EOSQL2
+        create extension if not exists "uuid-ossp" with schema actual_state;
+        create extension if not exists "btree_gist" with schema actual_state;
+        create extension if not exists "pg_trgm" with schema actual_state;
 EOSQL2
 
-if [ -n "$DB_UPGRADE_TO_SUPERUSER" ]; then
-    true "${DB_USER:?DB_USER is unset. Error.}"
+    if [ -n "$DB_UPGRADE_TO_SUPERUSER" ]; then
+        true "${DB_USER:?DB_USER is unset. Error.}"
 
-    echo
-    echo Warning: Upgrading $DB_USER to SUPERUSER.
-    echo
+        echo
+        echo Warning: Upgrading $DB_USER to SUPERUSER.
+        echo
 
-    psql -v ON_ERROR_STOP=1 <<-EOSQL3
-        ALTER ROLE $DB_USER WITH SUPERUSER;
+        psql -v ON_ERROR_STOP=1 <<-EOSQL3
+            ALTER ROLE $DB_USER WITH SUPERUSER;
 EOSQL3
+    else
+        echo Skipping upgrade to SUPERUSER.
+    fi
 else
-    echo Skipping upgrade to SUPERUSER.
+    echo Skipping creation of LoRA data db
 fi
 
 
--- docker-entrypoint-initdb.d/20-init-conf-db.sh
+++ docker-entrypoint-initdb.d/20-init-conf-db.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -e
 
-true "${CONF_DB_USER:?CONF_DB_USER is unset. Error!}"
-true "${CONF_DB_PASSWORD:?CONF_DB_PASSWORD is unset. Error!}"
-true "${CONF_DB_NAME:?CONF_DB_NAME is unset. Error!}"
+if [ -n "$CONF_DB_NAME" ]; then
 
-psql -v ON_ERROR_STOP=1 <<-EOSQL
-    create user $CONF_DB_USER with encrypted password '$CONF_DB_PASSWORD';
-    create database $CONF_DB_NAME owner $CONF_DB_USER;
-    grant all privileges on database $CONF_DB_NAME to $CONF_DB_USER;
+    true "${CONF_DB_USER:?CONF_DB_USER is unset. Error!}"
+    true "${CONF_DB_PASSWORD:?CONF_DB_PASSWORD is unset. Error!}"
+
+    echo Creating config db: $CONF_DB_NAME
+
+    psql -v ON_ERROR_STOP=1 <<-EOSQL
+        create user $CONF_DB_USER with encrypted password '$CONF_DB_PASSWORD';
+        create database $CONF_DB_NAME owner $CONF_DB_USER;
+        grant all privileges on database $CONF_DB_NAME to $CONF_DB_USER;
 EOSQL
+else
+    echo Skipping creation of config db
+fi
+
 
 # we can connect without password because ``trust`` authentication for Unix
 # sockets is enabled inside the container.
