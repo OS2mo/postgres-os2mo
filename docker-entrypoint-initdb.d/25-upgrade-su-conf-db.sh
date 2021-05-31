068b0f4bc6adc5e6099bd739ced31e041dab52dd
Emil Madsen <sovende@gmail.com>
2021-05-31 14:19:24 +0200

    [#43109] Convert image from Postgres to init container.
    Support updating passwords
    
    Update docker-entrypoint.sh
    
    Add support ssl
    
    Fix typo
    
    Fixup bad connection information
    
    Fix different dbnames
    
    Added grant to postgres user for each new user
    
    Battletested
    
    Fixup permissions
    
    Set sslmode from arg
    
    Remove config_db and session_db
    
    Added pgtap version
    
    Build test images too
    
    Build test images too

--- docker-entrypoint-initdb.d/25-upgrade-su-conf-db.sh
+++
@@ -1,9 +0,0 @@
-if [ -n "$CONF_DB_NAME" ]; then
-    echo
-    echo Warning: Upgrading $CONF_DB_USER to SUPERUSER.
-    echo
-
-    psql -v ON_ERROR_STOP=1 <<-EOSQL
-        ALTER ROLE $CONF_DB_USER WITH SUPERUSER;
-EOSQL
-fi
